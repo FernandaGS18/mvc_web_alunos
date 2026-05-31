6fe90d32f81120a512b6953780d5dfc01133afa6
Fernanda <fernandagdasilva4@gmail.com>
2026-05-31 23:53:47 +0000

    MVC Web Alunos finalizada

---
+++ src/main/java/com/exemplo/mvc/MvcApplication.java
@@ -0,0 +1,14 @@
+package com.exemplo.mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcApplication {
+
+    public static void main(String[] args) {
+        // Inicializa e executa a aplicação Spring Boot
+        SpringApplication.run(MvcApplication.class, args);
+    }
+
+}
--- src/main/java/com/exemplo/mvc/controller/AlunoController.java
+++ src/main/java/com/exemplo/mvc/controller/AlunoController.java
@@ -13,25 +13,24 @@ public class AlunoController {
 
     private List<Aluno> alunos = new ArrayList<>();
 
-    // Rota que exibe o formulário de cadastro
+    @GetMapping("/")
+    public String index() {
+        return "redirect:/alunos";
+    }
+
     @GetMapping("/alunos")
     public String formulario() {
         return "alunos-form";
     }
 
-    // Rota que recebe os dados do formulário e lista os alunos cadastrados
     @PostMapping("/alunos")
     public String cadastrar(
         @RequestParam String nome,
         @RequestParam String matricula,
         Model model
     ) {
-        // Cria o modelo e adiciona na lista simulada em memória
         alunos.add(new Aluno(nome, matricula));
-        
-        // Passa a lista atualizada para ser exibida na View
         model.addAttribute("alunos", alunos);
-        
         return "alunos-lista";
     }
 }
